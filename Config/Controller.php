c2dd836bd3eb5007c327bd7f01c0bc25e4a75497
Artem <artem.popas@gmail.com>
2024-12-08 01:34:34 +0200

    Lesson 7

---
+++ Config/Controller.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    '' => 'Main/index',
+    'main' => 'Main/index',
+    'main/create' => 'Main/create',
+    'main/delete' => 'Main/delete',
+    'main/update' => 'Main/update',
+
+    'about' => 'About/index',
+    'about/create' => 'About/create',
+    'about/delete' => 'About/delete',
+    'about/update' => 'About/update',
+
+    'gallery' => 'Gallery/index',
+    'gallery/create' => 'Gallery/create',
+    'gallery/delete' => 'Gallery/delete',
+    'gallery/update' => 'Gallery/update',
+
+    'error' => 'Error/index',
+    'error/create' => 'Error/create',
+    'error/delete' => 'Error/delete',
+    'error/update' => 'Error/update',
+];
